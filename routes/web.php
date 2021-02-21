db5f6f8b9fd91cfce8103de8bf421762fe186f38
ArturTrotsky <artur.trotsky@ukr.net>
2021-02-21 19:30:10 +0200

    push with gitkraken

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::get('/', [ObjectsController::class, 'index'])->middleware(['auth', 'verified']);
 
-
 Auth::routes(['verify' => true]);
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('/objects', ObjectsController::class);
